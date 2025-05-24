f76808c031b97328e6607debc4a45c348ba3f292
yeah1966 <92924742+yeah1966@users.noreply.github.com>
2025-05-24 16:18:55 +0200

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -17,9 +17,7 @@ export async function generateStaticParams(): Promise<{ slug: string }[]> {
   return slugs;
 }
 
-export async function generateMetadata(
-  { params }: { params: { slug: string } }
-) {
+export async function generateMetadata({ params }: { params: { slug: string } }) {
   const filePath = path.join(process.cwd(), "app", "blog", params.slug, "post.mdx");
 
   try {
@@ -35,9 +33,7 @@ export async function generateMetadata(
   }
 }
 
-export default async function BlogPostPage(
-  { params }: { params: { slug: string } }
-) {
+export default async function BlogPostPage({ params }: { params: { slug: string } }) {
   const filePath = path.join(process.cwd(), "app", "blog", params.slug, "post.mdx");
 
   try {
@@ -212,4 +208,5 @@ export default async function BlogPostPage(
     return notFound();
   }
 }
+
 
